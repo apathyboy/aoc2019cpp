aaf512396adfbb6feeba149627c2052198fbec93
eb <eb@2bros.dev>
2020-12-23 20:21:07 -0600

    day 01 part 1 and 2 solutions

--- days/day01/main.cpp
+++ days/day01/main.cpp
@@ -6,14 +6,35 @@
 namespace rs = ranges;
 namespace rv = ranges::views;
 
-int64_t part1()
+[[nodiscard]] auto generate_input(std::istream& input)
 {
-    return 0;
+    return rs::getlines(input)
+           | rv::transform([](auto&& s) { return std::stoi(s | rs::to<std::string>); })
+           | rs::to_vector;
 }
 
-int64_t part2()
+[[nodiscard]] int calculate_fuel(int mass) noexcept
 {
-    return 0;
+    return (mass / 3) - 2;
+}
+
+[[nodiscard]] int calculate_total_fuel(int module_mass) noexcept
+{
+    return rs::accumulate(
+        rv::iota(0)
+            | rv::exclusive_scan(module_mass, [](auto i, auto) { return calculate_fuel(i); })
+            | rv::tail | rv::take_while([](int i) { return i > 0; }),
+        0);
+}
+
+int64_t part1(std::vector<int> input)
+{
+    return rs::accumulate(input | rv::transform(calculate_fuel), 0);
+}
+
+int64_t part2(std::vector<int> input)
+{
+    return rs::accumulate(input | rv::transform(calculate_total_fuel), 0);
 }
 
 #ifndef UNIT_TESTING
@@ -22,12 +43,12 @@ int main()
 {
     fmt::print("Advent of Code 2019 - Day 01\n");
 
-    std::string input_path = "days/day01/puzzle.in";
+    std::ifstream ifs{"days/day01/puzzle.in"};
 
-    std::ifstream ifs{input_path};
+    auto input = generate_input(ifs);
 
-    fmt::print("Part 1 Solution: {}\n", part1());
-    fmt::print("Part 2 Solution: {}\n", part2());
+    fmt::print("Part 1 Solution: {}\n", part1(input));
+    fmt::print("Part 2 Solution: {}\n", part2(input));
 
     return 0;
 }
@@ -38,22 +59,17 @@ int main()
 #include <catch2/catch.hpp>
 #include <sstream>
 
-TEST_CASE("Can solve part 1 example")
+TEST_CASE("Can calculate fuel for mass")
 {
-    std::stringstream ss;
-
-    ss << R"()";
-
-    REQUIRE(0 == part1());
+    REQUIRE(2 == calculate_fuel(12));
+    REQUIRE(654 == calculate_fuel(1969));
 }
 
-TEST_CASE("Can solve part 2 example")
+TEST_CASE("Can calculate total fuel for mass")
 {
-    std::stringstream ss;
-
-    ss << R"()";
-
-    REQUIRE(0 == part2());
+    REQUIRE(2 == calculate_total_fuel(14));
+    REQUIRE(966 == calculate_total_fuel(1969));
+    REQUIRE(50346 == calculate_total_fuel(100756));
 }
 
 #endif
