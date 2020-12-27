0c5ab5827a536525e7b1d91dd61dd767add923e8
eb <eb@2bros.dev>
2020-12-26 18:47:27 -0600

    day 3 stub

---
+++ days/day03/main.cpp
@@ -0,0 +1,59 @@
+#include <fmt/format.h>
+#include <range/v3/all.hpp>
+
+#include <fstream>
+
+namespace rs = ranges;
+namespace rv = ranges::views;
+
+int64_t part1()
+{
+    return 0;
+}
+
+int64_t part2()
+{
+    return 0;
+}
+
+#ifndef UNIT_TESTING
+
+int main()
+{
+    fmt::print("Advent of Code 2019 - Day 03\n");
+
+    std::string input_path = "days/day03/puzzle.in";
+
+    std::ifstream ifs{input_path};
+
+    fmt::print("Part 1 Solution: {}\n", part1());
+    fmt::print("Part 2 Solution: {}\n", part2());
+
+    return 0;
+}
+
+#else
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <sstream>
+
+TEST_CASE("Can solve part 1 example")
+{
+    std::stringstream ss;
+
+    ss << R"()";
+
+    REQUIRE(0 == part1());
+}
+
+TEST_CASE("Can solve part 2 example")
+{
+    std::stringstream ss;
+
+    ss << R"()";
+
+    REQUIRE(0 == part2());
+}
+
+#endif
